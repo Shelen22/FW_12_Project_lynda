b8a57c701cf57234b258feb9e145e2ddccd63bad
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-11-11 11:26:36 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,10 +1,50 @@
 function payment(){
-    let form = document.getElementById('form');
-    let firstname = form.namef.value;
-    let lastname = form.namel.value;
-    let exp = form.expdate.value;
-    let cvv = form.cvv.value;
+    var form = document.getElementById('form');
+    var firstname = form.namef.value;
+    var lastname = form.namel.value;
+    var cardno = form.namec.value;
+    var expdate = form.expdate.value;
+    var cvv = form.cvv.value;
+    var pincode = form.pincode.value;
+    
 
-    console.log(firstname,lastname,exp,cvv)
+    if(firstname == ""){
+        console.log('invalid');
+        document.getElementById('wrongf').innerHTML = "First name Required";
+    }
+    else{
+        document.getElementById('wrongf').innerHTML="";
+    }
+
+    if(lastname == ""){
+        console.log('invalid');
+        document.getElementById('wrongl').innerHTML = "last name Required";
+    }
+    else{
+        document.getElementById('wrongl').innerHTML="";
+    }
+    if(cardno == "" || cardno.length != 16){
+        console.log('invalid');
+        document.getElementById('wrongc').innerHTML = "Please enter a valid card number"
+    }
+    else{
+        document.getElementById('wrongc').innerHTML="";
+    }
+    if(cvv == "" || cvv.length != 3){
+        console.log('invalid');
+        document.getElementById('wrongcvv').innerHTML = "Invalid security code";
+    }
+    else{
+        document.getElementById('wrongcvv').innerHTML="";
+    }
+
+    if(pincode == "" || pincode.length != 6){
+        console.log('invalid');
+        document.getElementById('wrongpin').innerHTML = "Please enter a valid post code";
+    }
+    else{
+        document.getElementById('wrongpin').innerHTML="";
+    }
+
 
 }
