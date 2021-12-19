3259fd5e5a85a1b0059df58fcdc923ad1935140f
Shailendra Singh <shailendrauday5@gmail.com>
2021-12-19 14:08:34 +0530

    login

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const coursescontroller = require('./controllers/course.controller');
 const searchcontroller = require('./controllers/search.controller');
 const dataRcontroller = require('./controllers/data_R.controller');
 const dataScontroller = require('./controllers/data_S.controller');
+const logincontroller = require('./controllers/user_login');
+
 
 
 
@@ -11,6 +13,7 @@ app.use("/course",coursescontroller);
 app.use("/search",searchcontroller);
 app.use("/data_R",dataRcontroller);
 app.use("/data_S",dataScontroller);
+app.use("/login", logincontroller);
 
 
 start();
